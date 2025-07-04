b5045bb2006d2e283685678f07e86d99a887b157
Chris Froeschl <chris.froeschl@br.de>
2025-07-04 10:25:27 +0200

    select relevant lean files for doc build

--- Elligator.lean
+++ Elligator.lean
@@ -1,3 +1,3 @@
 -- This module serves as the root of the `Elligator` library.
 -- Import modules here that should be built as part of the library.
-import «Elligator».Basic
+import «Elligator».Elligator1
