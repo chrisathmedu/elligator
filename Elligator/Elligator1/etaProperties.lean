8a40814a1f34f270638a7e4291047da3a673f86b
Chris Anto Froeschl <chris@chrisfroeschl.de>
2025-10-11 15:55:43 +0200

    η_ne_zero: adds statement and proof

--- Elligator/Elligator1/etaProperties.lean
+++ Elligator/Elligator1/etaProperties.lean
@@ -40,3 +40,35 @@ lemma η_eq_zero
     rw [ϕ_of_t_eq_zero_one t s s_h1 s_h2 q field_cardinality q_prime_power q_mod_4_congruent_3]
     simp
 
+lemma η_ne_zero
+  (s : F)
+  (s_h1 : s ≠ 0)
+  (s_h2 : (s^2 - 2) * (s^2 + 2) ≠ 0)
+  (q : ℕ)
+  (field_cardinality : Fintype.card F = q)
+  (q_prime_power : IsPrimePow q)
+  (q_mod_4_congruent_3 : q % 4 = 3)
+  (point : {p : F × F // p ∈ E_over_F s s_h1 s_h2 q field_cardinality q_prime_power q_mod_4_congruent_3})
+  (point_props : ϕ_over_F_props s s_h1 s_h2 q field_cardinality q_prime_power q_mod_4_congruent_3 point)
+  (x_ne_zero : point.val.1 ≠ 0)
+  :
+  let η_of_point := η q field_cardinality q_prime_power q_mod_4_congruent_3 point.val
+  η_of_point ≠ 0 := by
+    intro η_of_point
+    let x := point.val.1
+    let y := point.val.2
+    unfold η_of_point η
+    change (y - 1) / (2 * (y + 1)) ≠ 0
+    apply div_ne_zero 
+    · intro h1
+      have h2 : y ≠ 1 := by exact y_ne_one s s_h1 s_h2 q field_cardinality q_prime_power q_mod_4_congruent_3 point point_props x_ne_zero
+      have h2 : y = 1 := by
+        rw [← add_left_inj 1] at h1
+        simp at h1
+        exact h1
+      contradiction
+    · unfold ϕ_over_F_props at point_props
+      apply mul_ne_zero
+      · exact FiniteFieldBasic.two_ne_zero q field_cardinality q_prime_power q_mod_4_congruent_3
+      · exact point_props.1
+
